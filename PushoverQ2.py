7fd04cde82e0595707efccb118de1f1496939b71
JayJay <jarrry.c@gmail.com>
2018-08-09 17:55:59 +1000

    Debugged and fixed warnings and added exceptions

--- PushoverQ2.py
+++ PushoverQ2.py
@@ -1,35 +1,63 @@
-import httplib
+import http.client
 import urllib
 import time
 from sense_hat import SenseHat
+
 
 class PushoverSender:
     def __init__(self, user_key, api_key):
         self.user_key = user_key
         self.api_key = api_key
- 
+
     def send_notification(self, text):
-        conn = httplib.HTTPSConnection("api.pushover.net:443")
-        post_data = {'user': self.user_key, 'token': self.api_key, 'message': text}
-        conn.request("POST", "/1/messages.json",
-                     urllib.urlencode(post_data), {"Content-type": "application/x-www-form-urlencoded"})
+        conn = httplib.HTTPSConnection(
+            "api.pushover.net:443"
+            )
+        post_data = {
+            'user': self.user_key,
+            'token': self.api_key,
+            'message': text
+            }
+        conn.request(
+            "POST",
+            "/1/messages.json",
+            urllib.parse.urlencode(post_data),
+            {"Content-type": "application/x-www-form-urlencoded"}
+            )
         # print(conn.getresponse().read())
 
 
-m = PushoverSender("u3rxb16wvd6a65afq3ncdfyopt35id","apj8741z2fgjv1gtu9seybng46ybcg")
-sense = SenseHat()
-temp = None
+def tempCheck():
+    sense = SenseHat()
+    temp = None
+
+    while (True):
+        if(sense):
+            temp = sense.get_temperature()
+            try:
+                print("Temp is "+str(int(temp)))
+                if float(temp) < 20:
+                    return temp
+            except ValueError:
+                print('Non-numeric data found in the file.')
+            except TypeError:
+                print('a type error has occurred')
+
+            time.sleep(60)
 
 
-while (temp > 20 or temp == None):
-    if(sense):
-        temp = sense.get_temperature()
-        print("Temp is "+int(temp))
-        time.sleep(10)
-
-
-message = "Temperature is below 20, don't forget to bring a sweater (Temp: "+str(int(temp))+")"
-
-if (temp > 20):
+def sendMessenge(temp):
+    m = PushoverSender(
+        "u3rxb16wvd6a65afq3ncdfyopt35id",
+        "apj8741z2fgjv1gtu9seybng46ybcg"
+        )
+    message = "Temperature is below 20, don't forget to bring a sweater (Temp: "+str(int(temp))+")"
     m.send_notification(message)
-    time.sleep(60)
+
+
+def main():
+    temp = tempCheck()
+    sendMessenge(temp)
+
+
+main()
