572b34c54092a0942aa4fccad8c7517f4b1b19dc
JayJay <jarrry.c@gmail.com>
2018-08-16 18:32:13 +1000

    Flake8 save_name.py and bluetooth_Q3.py

--- bluetooth_Q3.py
+++ bluetooth_Q3.py
@@ -1,24 +1,25 @@
 import time
 import sqlite3
-import bluetooth, subprocess
+import bluetooth
 from sense_hat import SenseHat
 
 
-dbname='names.db'
+dbname = 'names.db'
+
 
 # get data from SenseHat sensor
-def getSenseHatData():	
+def getSenseHatData():
     sense = SenseHat()
     temp = sense.get_temperature()
-	
     if temp is not None:
         temp = round(temp, 1)
         return (temp)
 
+
 # get names from Database
-def getName():	
-    conn=sqlite3.connect(dbname)
-    curs=conn.cursor()
+def getName():
+    conn = sqlite3.connect(dbname)
+    curs = conn.cursor()
     names = []
     curs.execute("SELECT name FROM NAMES")
     rows = curs.fetchall()
@@ -26,9 +27,10 @@ def getName():
         print(row)
         names.append(row[0])
     conn.close()
-    print (names)
+    print(names)
     return names
 # code to get name
+
 
 # Search for device based on device's name
 def search(device_names):
@@ -36,28 +38,35 @@ def search(device_names):
         device_address = None
         dt = time.strftime("%a, %d %b %y %H:%M:%S", time.localtime())
         print("\nCurrently: {}".format(dt))
-        time.sleep(3) #Sleep three seconds 
+        # Sleep three seconds
+        time.sleep(3)
         nearby_devices = bluetooth.discover_devices()
-
         for mac_address in nearby_devices:
             for i in range(len(device_names)):
-                if device_names[i] == bluetooth.lookup_name(mac_address, timeout=5):
+                if device_names[i] == bluetooth.lookup_name(
+                        mac_address,
+                        timeout=5
+                        ):
                     device_address = mac_address
                     name = device_names[i]
                     break
-            break        
+            break
         if device_address is not None:
             return name
-    
         else:
             print("Could not find target device nearby...")
+
 
 def main():
     sense = SenseHat()
     names = getName()
     name = search(names)
     temp = getSenseHatData()
-    if name != None:
-        sense.show_message("Hi {}! Current Temp is {}*c".format(name, temp), scroll_speed=0.05)
+    if name is not None:
+        sense.show_message(
+            "Hi {}! Current Temp is {}*c".format(name, temp),
+            scroll_speed=0.05
+            )
+
 
 main()
--- save_name.py
+++ save_name.py
@@ -1,8 +1,23 @@
 import sqlite3
 
-dbname = "names.db"
-conn=sqlite3.connect(dbname)
-curs=conn.cursor()
-curs.execute("""INSERT INTO NAMES ('name') values ('JayJayC')""")
-conn.commit()
-conn.close()
+
+def getInput():
+    name = input("Add phone's name to raspberry pi's database: ")
+    return name
+
+
+def setName(user_name):
+    query = """INSERT INTO NAMES ('name') values ('%s')"""
+    dbname = "names.db"
+    conn = sqlite3.connect(dbname)
+    curs = conn.cursor()
+    curs.execute(query % (user_name))
+    conn.commit()
+    conn.close()
+
+
+def main():
+    setName(getInput())
+
+
+main()
